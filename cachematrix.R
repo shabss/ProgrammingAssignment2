ecca22c2cfb94204b256975e93a1c24cb748887f
unknown <ssuterwala@USMONSSUTERWA01.infor.com>
2014-04-25 15:55:50 -0700

    removing unnessary comments in makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -5,22 +5,6 @@
 ## to get a cached matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
-    ##
-    ## makeVector <- function(x = numeric()) {
-    ##     m <- NULL
-    ##     set <- function(y) {
-    ##         x <<- y
-    ##         m <<- NULL
-    ##     }
-    ##     get <- function() x
-    ##     setmean <- function(mean) m <<- mean
-    ##     getmean <- function() m
-    ##     list(set = set, get = get,
-    ##          setmean = setmean,
-    ##          getmean = getmean)
-    ## }
-    ##
     
     s <- NULL
     set <- function(y) {
